55463bd49ac2c041098da38cb1b97a913d6f60a8
chengdu <3281328128@qq.com>
2020-03-20 20:24:00 +0800

    add excel2003 test

--- src/test/java/com/scd/test/ExcelExportTest.java
+++ src/test/java/com/scd/test/ExcelExportTest.java
@@ -107,4 +107,29 @@ public class ExcelExportTest {
         excelExport.close();
         System.out.println("time " + (System.currentTimeMillis() -  startTime) + " ms");
     }
+
+    @Test
+    public void testExcel2003Export() {
+        long startTime = System.currentTimeMillis();
+        String exportPath = "file" + File.separator + UUID.randomUUID().toString();
+        File file = new File(exportPath);
+        file.mkdirs();
+        String filePath = exportPath + File.separator + "test.xls";
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BatchLabelMapper batchLabelMapper = sqlSession.getMapper(BatchLabelMapper.class);
+        ExportParam exportParam = new ExportParam();
+        Map<String, Object> searchParam = new HashMap<>(16);
+        searchParam.put("code", 500000);
+        searchParam.put("tableName", "label_2");
+        exportParam.setHeader("id, parent, code, status, type, customer_id, operator_id, reseller_id, user_id, product_id, batch, query_times," +
+                "first_time, create_time, update_time");
+        exportParam.setSum(batchLabelMapper.countTbSum(searchParam));
+        exportParam.setPageSize(10000);
+        exportParam.setSheetName("test2003");
+        exportParam.setSearchParam(searchParam);
+        ExcelQueryExport excelExport = new ExcelQueryExport(filePath, null, false, exportParam);
+        excelExport.exportQueryPage(batchLabelMapper::selectPage);
+        excelExport.close();
+        System.out.println("time " + (System.currentTimeMillis() -  startTime) + " ms");
+    }
 }
